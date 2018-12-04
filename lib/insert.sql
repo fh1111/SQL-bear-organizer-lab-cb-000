36189436de93561f3b0f1b9e98dad81496f3e46f
Fer Hernandez <fh.1111@gmail.com>
2018-12-04 01:39:39 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ('Mr. Chocolate', 20,'M', 'Brown','Crazy' , 1);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ('Rowdy', 4,'M', 'Black','Lazy' , 0);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ('Tabitha', 5,'F', 'Brown','Crazy' , 1);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ('Sergeant Brown', 6,'M', 'Black','Lazy' , 0);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ('Melissa', 7,'F', 'Brown','Crazy' , 1);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ('Grinch', 8,'M', 'Black','Lazy' , 0);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ('Wendy', 9,'F', 'Brown','Crazy' , 1);
+
+INSERT INTO bears ( age, gender,color,temperament,alive)
+VALUES ( 10,'M', 'Black','Lazy' , 0);
